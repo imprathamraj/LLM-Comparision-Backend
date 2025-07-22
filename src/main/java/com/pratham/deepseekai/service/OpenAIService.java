eb80ac6a8a17b0f26add9da2ea272713fc558662
Pratham Raj <rjpratham04@gmail.com>
2025-07-22 23:42:32 +0530

    [Pratham Raj].Add OpenAIService

--- src/main/java/com/pratham/deepseekai/service/OpenAIService.java
+++ src/main/java/com/pratham/deepseekai/service/OpenAIService.java
@@ -16,9 +16,11 @@ public class OpenAIService {
 			// String response = chatModel.call(message);
 			// Replace "Needs bucks to run the OpenAI show!" from below line with response
 			// when OpenAI API Key is available(Application.properties).
+
 			return new ResponseEntity<>("Needs money to buy the OpenAI API Key!", HttpStatus.OK);
 		} catch (Exception e) {
 			e.printStackTrace();
+
 			return new ResponseEntity<>("No record found! Just ask easy question.", HttpStatus.INTERNAL_SERVER_ERROR);
 		}
 
