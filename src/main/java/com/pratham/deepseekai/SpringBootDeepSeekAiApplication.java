d57a1afecd7aa1f18b020c4aaae2d2bec17251fa
Pratham Raj <rjpratham04@gmail.com>
2025-07-22 23:48:06 +0530

    [Pratham Raj].Add SpringBootDeepSeekAiApplication

---
+++ src/main/java/com/pratham/deepseekai/SpringBootDeepSeekAiApplication.java
@@ -0,0 +1,13 @@
+package com.pratham.deepseekai;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootDeepSeekAiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootDeepSeekAiApplication.class, args);
+	}
+
+}
