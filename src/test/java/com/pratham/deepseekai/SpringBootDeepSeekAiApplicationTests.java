6ce70419855e4f4e7b880789ad7de84d4d703b65
Pratham Raj <rjpratham04@gmail.com>
2025-07-22 23:47:21 +0530

    [Pratham Raj].Add SpringBootDeepSeekAiApplicationTests

---
+++ src/test/java/com/pratham/deepseekai/SpringBootDeepSeekAiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.pratham.deepseekai;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootDeepSeekAiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
